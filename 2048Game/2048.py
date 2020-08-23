5b4476a0d4901810c23418dbd603b575e5cefa78
Vishesh <vishesh.dh1@gmail.com>
2020-08-23 14:23:55 +0530

    UI work in progress

--- 2048Game/2048.py
+++ 2048Game/2048.py
@@ -1,136 +1,64 @@
-import random
-def start_game():
-    mat = []
-    for i in range(4):
-        mat.append([0]*4)
-    return mat
+from Tkinter import Frame, Label, CENTER
+import constants as c
+import Logic
 
-def add_new_2(mat):
-    r = random.randint(0, 3)
-    c = random.randint(0, 3)
-    
-    while mat[r][c] != 0:
-        r = random.randint(0, 3)
-        c = random.randint(0, 3)
+
+class Game2048(Frame):
+    def __init__(self):
+        Frame.__init__(self)
         
-    mat[r][c] = 2
-
-def reverse(mat):
-    new_mat = []
-    for i in range(4):
-        new_mat.append([])
-        for j in range(4):
-            new_mat[i].append(mat[i][3-j])
-            
-    return new_mat
-
-def transpose(mat):
-    new_mat = []
-    for i in range(4):
-        new_mat.append([])
-        for j in range(4):
-            new_mat[i].append(mat[j][i])
-    
-    return new_mat
+        self.grid()
+        self.master.title('2048')
+        self.master.bind("<Key>", self.key_down)
+        self.commands = {
+            c.KEY_UP: Logic.move_up, c.KEY_DOWN: Logic.move_down
+            c.KEY_LEFT: Logic.move_left, c.KEY_RIGHT: Logic.move_right
+        }
         
-def merge(mat):
-    changed = False
-    
-    for i in range(4):
-        for j in range(3):
-            if mat[i][j] == mat[i][j+1] and mat[i][j] != 0:
-                mat[i][j] = mat[i][j]*2
-                mat[i][j+1] = 0
-                changed = True
-                
-    return mat, changed
-
-
-def compress(mat):
-    changed = False
-    new_mat = []
-    for i in range(4):
-        new_mat.append([0]*4)
-    
-    for i in range(4):
-        pos = 0
-        for j in range(4):
-            if mat[i][j] != 0:
-                new_mat[i][pos] = mat[i][j]
-                if j!=pos:
-                    changed = True
-                pos += 1
-    
-    return new_mat, changed
-
-
-def move_up(grid):
-    transposed_grid = transpose(grid)
-    new_grid, changed1 = compress(transposed_grid)
-    new_grid, changed2 = merge(new_grid)
-    changed = changed1 or changed2
-    
-    new_grid = compress(new_grid)
-    final_grid = transpose(new_grid)
-    return final_grid, changed
-
-def move_down(grid):
-    transposed_grid = transpose(grid)
-    reversed_grid = reverse(transposed_grid)
-    new_grid, changed1 = compress(reversed_grid)
-    new_grid, changed2 = merge(new_grid)
-    changed = changed1 or changed2
-    
-    new_grid = compress(new_grid)
-    final_reversed_grid = reverse(new_grid)
-    final_grid = transpose(final_reversed_grid)
-    return final_grid, changed
-    
-
-def move_right(grid):
-    reversed_grid = reverse(grid)
-    new_grid, changed1 = compress(reversed_grid)
-    new_grid, changed2 = merge(new_grid)
-    changed = changed1 or changed2
-    
-    new_grid = compress(new_grid)
-    final_grid = reverse(new_grid)
-    return final_grid, changed
-
-def move_left(grid):
-    new_grid, changed1 = compress(grid)
-    new_grid, changed2 = merge(new_grid)
-    changed = changed1 or changed2
-    
-    new_grid, temp = compress(new_grid)
-    return new_grid, changed
-
-def get_current_state(mat):
-    # anywhere 2048 is present
-    for i in range(4):
-        for j in range(4):
-            if mat[i][j] == 2048:
-                return 'WON'
+        self.grid_cells = []
+        self.init_grid()
+        self.init_matrix()
+        self.update_grid_cells()
+        
+        self.mainloop()
+        
+    def init_grid(self):
+        background = Frame(self, bg=c.BACKGROUND_COLOR_GAME,
+                           width=c.SIZE, height=c.SIZE)
+        
+        background.grid()
+        
+        for i in range(c.GRID_LEN):
+            grid_row = []
+            for j in range(c.GRID_LEN):
+                cell = Frame(background, bg=c.BACKGROUND_COLOR_CELL_EMPTY,
+                             width = c.SIZE / c.GRID_LEN,
+                             height = c.SIZE / c.GRID_LEN)
+                cell.grid(row=i, column=j, padx=c.GRID_PADDING,
+                          pady=c.GRID_PADDING)
+                t = Label(master=cell, text="",
+                          bg=c.BACKGROUND_COLOR_CELL_EMPTY,
+                          justify=CENTER, font=c.FONT, width=5, height=2)
+                t.grid()
+                grid_row.append(t)
             
-    # anywere either 0 or same numbers next to each other are present
-    for i in range(4):
-        for j in range(4):
-            if mat[i][j] == 0:
-                return 'GAME NOT OVER'
-            
-            if i < 3 and mat[i+1][j]:
-                return 'GAME NOT OVER'
-            
-            if j < 3 and mat[i][j+1]:
-                return 'GAME NOT OVER'
-            
-    return 'LOST'
-
-mat = start_game()
-add_new_2(mat)
-add_new_2(mat)
-print(mat)
-mat = move_up(mat)
-print(mat)
-mat = move_right(mat)
-print(mat)
+            self.grid_cells.append(grid_row)
+        
+    def init_matrix(self):
+        self.matrix = Logic.start_game()
+        Logic.add_new_2(self.matrix)
+        Logic.add_new_2(self.matrix)
+        
+    def update_grid_cells(self):
+        for i in range(c.GRID_LEN):
+            for j in range(c.GRID_LEN):
+                new_number = self.matrix[i][j]
+                if new_number == 0:
+                    self.grid_cells[i][j].configure(
+                        text="", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                else:
+                    self.grid_cells[i][j].configure(text=str(new_number),
+                        bg = c.BACKGROUND_COLOR_DICT[new_number],
+                        fg=c.CELL_COLOR_DICT[new_number])
+                    
+        self.update_idletasks()
