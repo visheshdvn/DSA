986ddd2140847135b37410a1a8e747829e17f6a8
visheshdvn <vishesh.dh1@gmail.com>
2021-09-03 01:23:04 +0530

    link-list-2 | delNode | bug fix

--- 2linkedlist2/delNnode.py
+++ 2linkedlist2/delNnode.py
@@ -3,6 +3,16 @@ class Node:
         self.data = data
         self.next = None
 
+
+def deleteHelper(head, N):
+    t = head
+    while N:
+        if t.next == None:
+            return None
+        t = t.next
+        N-=1
+    return t
+
 def skipMdeleteN(head, M, N):
     if M == N == 0:
         return None
@@ -12,43 +22,27 @@ def skipMdeleteN(head, M, N):
 
     if M == 0 and N != 0:
         return None
-        
+
     nhead = Node(0)
     nhead.next = head
-    dl = N - 1
-    skip = M
-    node = nhead
-
-    while skip:
-        if node == None:
-            return head
-        node = node.next
-        skip -= 1
-
-    if node == None:
-        return head
-
-    skipped = node.next
-    node.next = None
-
-    while dl:
-        if skipped == None:
-            return head
-        skipped = skipped.next
-        dl -= 1
     
-    if skipped == None:
-        return head
+    val = 0
+    while nhead.next:
+        val+=1
+        nhead = nhead.next
+        if nhead == None:
+            break
+        if val != M:
+            continue
+        
+        node = deleteHelper(nhead, N+1)
+        nhead.next = node
+        val = 0
     
-    smallhead = skipMdeleteN(skipped.next, M, N)
-
-    node.next = smallhead
-
-    return nhead.next
-
+    return head
 
 def ll(arr):
-    if len(arr)==0:
+    if len(arr) == 0:
         return None
     head = Node(arr[0])
     last = head
@@ -57,18 +51,20 @@ def ll(arr):
         last = last.next
     return head
 
+
 def printll(head):
     while head:
         print(head.data, end=' ')
         head = head.next
     print()
 
+
 # Main
 # Read the link list elements including -1
-arr=list(int(i) for i in input().strip().split(' '))
+arr = list(int(i) for i in input().strip().split(' '))
 # Create a Linked list after removing -1 from list
 l = ll(arr[:-1])
-m=int(input())
-n=int(input())
+m = int(input())
+n = int(input())
 l = skipMdeleteN(l, m, n)
 printll(l)
