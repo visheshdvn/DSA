b717b15adda3cc7a0c00c503f7a8832e59dc9d56
visheshdvn <vishesh.dh1@gmail.com>
2021-09-13 01:03:26 +0530

    merge sort corrected

--- 0Recursion2/mergeSort.py
+++ 0Recursion2/mergeSort.py
@@ -1,35 +1,55 @@
-def mergeSort(arr):
-    if len(arr)>1:
-        mid = len(arr)//2
-        lefthalf = arr[:mid]
-        righthalf = arr[mid:]
+def merge(arr, l, mid, r):
+    n1 = mid-l+1
+    n2 = r-mid
 
-        mergeSort(lefthalf)
-        mergeSort(righthalf)
-        i=j=k=0
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
-                arr[k]=lefthalf[i]
-                i=i+1
-            else:
-                arr[k]=righthalf[j]
-                j=j+1
-            k=k+1
+    lArr = arr[l:mid+1]
+    rArr = arr[mid+1:r+1]
 
-        while i < len(lefthalf):
-            arr[k]=lefthalf[i]
-            i=i+1
-            k=k+1
+    i = 0
+    j = 0
+    k = l
+    
+    while i < n1 and j < n2:
+        if lArr[i] < rArr[j]:
+            arr[k] = lArr[i]
+            i += 1
+        elif lArr[i] > rArr[j]:
+            arr[k] = rArr[j]
+            j += 1
+        else:
+            arr[k] = lArr[i]
+            i+=1
+            k+=1
+            arr[k] = rArr[j]
+            j+=1
+            k+=1
+            continue
+        k += 1
 
-        while j < len(righthalf):
-            arr[k]=righthalf[j]
-            j=j+1
-            k=k+1
-        
-# Main
-n=int(input())
-arr=list(int(i) for i in input().strip().split(' '))
-mergeSort(arr)
+    while i < n1:
+        arr[k] = lArr[i]
+        i += 1
+        k+=1
+
+    while j < n2:
+        arr[k] = rArr[j]
+        j += 1
+        k+=1
+
+
+def mergeSort(arr, l, r):
+    if l < r:
+        m = l+(r-l)//2
+
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+        merge(arr, l, m, r)
+
+
+        # Main
+n = int(input())
+arr = list(int(i) for i in input().strip().split(' '))
+mergeSort(arr, 0, len(arr)-1)
 
 for i in arr:
-    print(i, end = ' ')
+    print(i)
