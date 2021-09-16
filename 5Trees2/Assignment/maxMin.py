e9ca3cae004277eafbb9f6990bad131bb757c1cf
visheshdvn <vishesh.dh1@gmail.com>
2021-09-16 17:13:56 +0530

    max min trees2 assignment modified

--- 5Trees2/Assignment/maxMin.py
+++ 5Trees2/Assignment/maxMin.py
@@ -17,38 +17,35 @@
 
 
 import queue
+
+
 class BinaryTreeNode:
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
+
 INT_MIN = -2147483648
 INT_MAX = 2147483647
 # Problem ID 1567, Find the minimum and maximum value
+
+
 def minMax(root):
-    
     if root == None:
-        return None, None
-    
-    max_left, min_left = minMax(root.left)
-    max_right, min_right = minMax(root.right)
-    
-    items = [max_left, min_left, max_right, min_right]
-    
-    to_return = [root.data]
-    for i in items:
-        if i != None:
-            to_return.append(i)
-            
-    return max(to_return), min(to_return)
-    
-    
+        return INT_MAX, INT_MIN
+
+    lmin, lmax = minMax(root.left)
+    rmin, rmax = minMax(root.right)
+    n = root.data
+
+    return min(rmin, lmin, n), max(lmax, rmax, n)
+
 
 def buildLevelTree(levelorder):
     index = 0
     length = len(levelorder)
-    if length<=0 or levelorder[0]==-1:
+    if length <= 0 or levelorder[0] == -1:
         return None
     root = BinaryTreeNode(levelorder[index])
     index += 1
@@ -60,19 +57,19 @@ def buildLevelTree(levelorder):
         index += 1
         if leftChild != -1:
             leftNode = BinaryTreeNode(leftChild)
-            currentNode.left =leftNode
+            currentNode.left = leftNode
             q.put(leftNode)
         rightChild = levelorder[index]
         index += 1
         if rightChild != -1:
             rightNode = BinaryTreeNode(rightChild)
-            currentNode.right =rightNode
+            currentNode.right = rightNode
             q.put(rightNode)
     return root
+
 
 # Main
 levelOrder = [int(i) for i in input().strip().split()]
 root = buildLevelTree(levelOrder)
 maximum, minimum = minMax(root)
 print(maximum, minimum)
-
