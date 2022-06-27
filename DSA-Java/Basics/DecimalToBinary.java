397d11ca84a3cece8a936b4f08cb18d5ae30205d
Vishesh Dhawan <vishesh.dh1@gmail.com>
2022-06-28 01:54:58 +0530

    Basic Java Practice questions

--- DSA-Java/Basics/DecimalToBinary.java
+++ DSA-Java/Basics/DecimalToBinary.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class DecimalToBinary {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
 
+        String binaryStr = "";
+        while (n > 0) {
+            binaryStr = (n & 1) + binaryStr;
+            n = n >> 1;
+        }
+
+        System.out.println(binaryStr);
     }
 }
