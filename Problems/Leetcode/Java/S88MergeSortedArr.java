c23896b294f5e813b36ef47ae192dee13af9d54d
Vishesh Dhawan <vishesh.dh1@gmail.com>
2023-08-15 01:54:22 +0530

    folder restructuring

--- Problems/Leetcode/Java/S88MergeSortedArr.java
+++ Problems/Leetcode/Java/S88MergeSortedArr.java
@@ -8,8 +8,8 @@ public class S88MergeSortedArr {
         int index = nums1.length - 1,
                 a = m - 1,
                 b = n - 1;
-        
-        while (a >= 0 && b>=0) {
+
+        while (a >= 0 && b >= 0) {
             if (nums2[b] > nums1[a]) {
                 nums1[index] = nums2[b];
                 b--;
@@ -20,7 +20,7 @@ public class S88MergeSortedArr {
             index--;
         }
 
-        while (b>=0) {
+        while (b >= 0) {
             nums1[index] = nums2[b];
             b--;
             index--;
@@ -30,10 +30,9 @@ public class S88MergeSortedArr {
     // take element input
     public static void populateArr(int[] arr, int startIndex, int length) {
         Scanner scan = new Scanner(System.in);
-        for (int i = startIndex; i < length; i++) {
+        for (int i = startIndex; i < startIndex + length; i++) {
             arr[i] = scan.nextInt();
         }
-        scan.close();
     }
 
     public static void printArr(int[] arr) {
@@ -45,22 +44,21 @@ public class S88MergeSortedArr {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         System.out.print("length of l1: ");
         int n = scan.nextInt();
         int arr1[] = new int[n];
 
         System.out.print("Numbers of elements in array 1: ");
         int n1 = scan.nextInt();
-        scan.close();
-
+        
         System.out.print("Enter arr1 elements: ");
         populateArr(arr1, 0, n1);
-
+        
         int arr2[] = new int[n - n1];
         System.out.print("Enter arr2 elements: ");
         populateArr(arr2, 0, n - n1);
-
+        
+        scan.close();
         merge(arr1, n1, arr2, n - n1);
         printArr(arr1);
         // printArr(arr2);
