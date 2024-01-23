dcccfb828a1534d1b7aefed55f7d03ca649764b4
Vishesh Dhawan <vishesh.dh1@gmail.com>
2024-01-23 10:06:43 +0000

    code cleanup

--- DSA/3Recursion/Recursion1/LastIndex.py
+++ DSA/3Recursion/Recursion1/LastIndex.py
@@ -11,24 +11,8 @@ def LastIndex(arr, x, li):
 # Main
 from sys import setrecursionlimit
 setrecursionlimit(11000)
+
 n=int(input())
 arr=list(int(i) for i in input().strip().split(' '))
 x=int(input())
 print(LastIndex(arr, x, -1))
-
-# Copying smaller list way
-
-# def LastIndex(a, x):
-#     l == len(a)
-#     if l == 0:
-#         return -1
-    
-#     smallerListOutput = LastIndex(a[1:], x)
-    
-#     if smallerListOutput != -1:
-#         return smallerListOutput + 1
-#     else:
-#         if a[0] == x:
-#             return 0
-#         else:
-#             return -1
--- DSA/3Recursion/Recursion1/firstIndex.py
+++ DSA/3Recursion/Recursion1/firstIndex.py
@@ -1,33 +1,18 @@
 def firstIndex(arr, x, fi):
     if fi == len(arr):
         return -1
+    
     if arr[fi] == x:
         return fi
         
-    return 0 + firstIndex(arr, x, fi+1)
+    return firstIndex(arr, x, fi+1)
 
 # Main
 from sys import setrecursionlimit
 setrecursionlimit(11000)
-n=int(input())
+
+print("Enter elements of Array: ")
 arr=list(int(i) for i in input().strip().split(' '))
 x=int(input())
+
 print(firstIndex(arr, x, 0))
-
-
-
-# Copying snaller list way
-
-# def firstIndex(a, x):
-#     l = len(a)
-#     if l == 0:
-#         return -1
-    
-#     if a[0] == x:
-#         return 0
-#     smallerListOutput = firstIndex(a[1:], x)
-    
-#     if smallerListOutput == -1:
-#         return -1
-#     else:
-#         return smallerListOutput + 1
